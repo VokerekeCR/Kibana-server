4ae2c81767e10602aeeff0e2301f5c7161da950c
VokerekeCR <victor.okereke@cloudreach.com>
2022-04-29 09:53:18 -0400

    Added correct key pair

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "ta-challenge-elk-team-3"
-    key    = "sprint3/challenge/elk-stack/terraform.tfstates"
-    dynamodb_table = "terraform-lock"
+    key    = "sprint3/challenge/elk-kibana/terraform.tfstate"
+    #dynamodb_table = "terraform-lock"
   }
 }
