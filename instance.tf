d0c1ca7e3f6b81e25f3c04464d01a10a8ec900f6
VokerekeCR <victor.okereke@cloudreach.com>
2022-05-03 15:26:36 -0400

    Adding Kibana ansible

--- instance.tf
+++ instance.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "kibana" {
     vpc_security_group_ids = [aws_security_group.kibana_sg.id]
     key_name               = var.my_keypair
     availability_zone = "us-east-2a"
+    associate_public_ip_address = true
     
 
     tags = {
