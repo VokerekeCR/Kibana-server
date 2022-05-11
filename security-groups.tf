133b58425d486a6875a7aec8dc17fb5262b30232
VokerekeCR <victor.okereke@cloudreach.com>
2022-05-11 09:43:11 -0400

    Final changes to Kibana ansible config

--- security-groups.tf
+++ security-groups.tf
@@ -15,9 +15,27 @@ resource "aws_security_group" "kibana_sg" {
   }
 
   ingress {
+    description     = "allow Kibana access to server"
+    from_port       = 1024
+    to_port         = 65535
+    protocol        = "tcp"
+    cidr_blocks = ["192.168.0.0/16"]
+
+  }
+
+  ingress {
     description     = "For connection to Kibana"
     from_port       = 5601
     to_port         = 5601
+    protocol        = "tcp"
+    cidr_blocks = ["192.168.0.0/16"]
+
+  }
+
+  ingress {
+    description     = "For connection to Kibana"
+    from_port       = 80
+    to_port         = 80
     protocol        = "tcp"
     cidr_blocks = ["192.168.0.0/16"]
 
